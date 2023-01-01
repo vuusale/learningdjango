bb24c0f93aedc428f11f1a435eb876f5f774015a
Vusala <you@example.com>
2023-01-01 18:04:08 +0400

    Until 2:06

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
     # return HttpResponse('<h1>Hello World!</h1>')
-    return render(request, "home.html", {})
+    return render(request, 'home.html', {})
 
 def contact_view(request, *args, **kwargs):
-    return render(request, "contact.html", {})
+    return render(request, 'contact.html', {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, "contact.html", {})
+    my_context = {
+        'title': 'this is about us',
+        'my_number': 123,
+        'my_list': [123, 4242, 123123],
+        'input':'<b>User input</b>',
+    }
+    return render(request, 'about.html', my_context)
